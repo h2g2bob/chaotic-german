0f67c85a6733a83d8fd34eadabc5816da0c06fbd
user <user@lorbanery>
2016-07-30 16:59:44 +0100

    Count the words in the subtitle files

---
+++ wordcount.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+import operator
+import os
+import os.path
+import re
+
+
+def words_from_file(f):
+	for line in f:
+		for word in re.findall(ur'\b\w+\b', line.decode('utf8'), re.U):
+			yield word
+
+
+def words_in_all_files():
+	PATH = u"./subtitle_files/"
+	for filename in os.listdir(PATH):
+		with file(os.path.join(PATH, filename)) as f:
+			for word in words_from_file(f):
+				yield word
+
+
+def words_by_frequency():
+	words = defaultdict(int)
+	for word in words_in_all_files():
+		words[word] += 1
+
+	return tuple(sorted(words.items(), key=operator.itemgetter(1), reverse=True))
+
+
+def main():
+	for word, count in words_by_frequency():
+		print word.encode("utf8"), count
+
+
+if __name__ == '__main__':
+	main()
